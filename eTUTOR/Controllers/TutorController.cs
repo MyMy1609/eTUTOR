5aadd766a8cd122c848520f389d3b7a26a8e047f
Nhựt <nhutduong3@vanlanguni.vn>
2019-02-27 21:24:30 +0700

    SearchTutor

--- eTUTOR/Controllers/TutorController.cs
+++ eTUTOR/Controllers/TutorController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eTUTOR.Models;
 
 namespace eTUTOR.Controllers
 {
     public class TutorController : Controller
     {
+        eTUITOREntities tutor = new eTUITOREntities();
         // GET: Tutor
         public ActionResult ListOfTutors()
         {
@@ -24,6 +26,12 @@ namespace eTUTOR.Controllers
         public ActionResult RegisterTutor()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult SearchTutor(string search)
+        {
+            var t = tutor.tutors.ToList().Where(x => x.fullname.Contains(search));
+            return View(t);
         }
     }
 }
