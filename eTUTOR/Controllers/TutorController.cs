03f9f7283da95e121b784bb8142704e3ff6d3dfa
Nhựt <nhutduong3@vanlanguni.vn>
2019-02-27 21:24:59 +0700

    Revert "SearchTutor"
    This reverts commit 5aadd766a8cd122c848520f389d3b7a26a8e047f.

--- eTUTOR/Controllers/TutorController.cs
+++ eTUTOR/Controllers/TutorController.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using eTUTOR.Models;
 
 namespace eTUTOR.Controllers
 {
     public class TutorController : Controller
     {
-        eTUITOREntities tutor = new eTUITOREntities();
         // GET: Tutor
         public ActionResult ListOfTutors()
         {
@@ -26,12 +24,6 @@ namespace eTUTOR.Controllers
         public ActionResult RegisterTutor()
         {
             return View();
-        }
-        [HttpPost]
-        public ActionResult SearchTutor(string search)
-        {
-            var t = tutor.tutors.ToList().Where(x => x.fullname.Contains(search));
-            return View(t);
         }
     }
 }
