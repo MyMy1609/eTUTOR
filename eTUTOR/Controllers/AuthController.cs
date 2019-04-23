3223c738a125c86c7a96030101c1dc69a021d286
tranphuhoa <tranphuhoa1995@gmail.com>
2019-04-23 13:46:14 +0700

    create AuthController

---
+++ eTUTOR/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace eTUTOR.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult TokenStream()
+        {
+            String api_key = "apiKey123123";
+            String api_secret = "MYCUSTOMCODELONGMOD4NEEDBEZE";
+
+            var signingKey = Convert.FromBase64String(api_secret);
+
+            JwtHeader jwtHeader = new JwtHeader(
+               new SigningCredentials(
+                   new SymmetricSecurityKey(signingKey),
+                   SecurityAlgorithms.HmacSha256
+               )
+            );
+
+            JwtPayload jwtPayload = new JwtPayload {
+                {"iss", api_key},
+                {"aud", api_key},
+                { "exp", ((DateTimeOffset)DateTime.UtcNow).AddMilliseconds(10800000).ToUnixTimeMilliseconds() }
+            };
+
+            var jwt = new JwtSecurityToken(jwtHeader, jwtPayload);
+            var jwtHandler = new JwtSecurityTokenHandler();
+            Console.Write(jwtHandler.WriteToken(jwt));
+
+            return Json(new { TokenContext= jwtHandler.WriteToken(jwt) }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
